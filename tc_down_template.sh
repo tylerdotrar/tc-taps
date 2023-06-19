6e19574b0fc9a1a9dc8117966a89825b9f13e51c
unknown <kushslayer69420@usa.com>
2023-06-19 01:05:29 -0400

    Increased verbosity

--- tc_down_template.sh
+++ tc_down_template.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
 # Author: Tyler McCann (@tylerdotrar)
-# Arbitrary Version Number: 1.0.0
+# Arbitrary Version Number: 1.0.1
 # Link: https://github.com/tylerdotrar/tc-taps
 
-# If 'network_taps' no longers exists, you can verify active TAPs with the following command:
-# tc qdisc show | grep ingress | awk '{print $5}' > network_taps
+###
+#    If 'network_taps' no longers exists, you can verify active TAPs with the following command:
+#
+#    tc qdisc show | grep ingress | awk '{print $5}' > network_taps
+###
 
-echo "Disabling TC Network TAPs:"
+echo "Disabling TC Network TAP(s):"
 
 # Read every active TAP interface in 'network_taps' and Murder Death Kill them
 while read i;
--- tc_up_template.sh
+++ tc_up_template.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Author: Tyler McCann (@tylerdotrar)
-# Arbitrary Version Number: 1.0.0
+# Arbitrary Version Number: 1.1.1
 # Link: https://github.com/tylerdotrar/tc-taps
 
 ###
@@ -13,29 +13,37 @@
 #    LXC Example:     ID: 203 ; net0: vmbr9
 #    Network Tap(s):  veth203i0
 #
-#    Check With:      ip --brief a | grep 'tap\|veth' | sed 's/@.*//g'
+#    Check With:      ip --brief a | grep 'tap\|veth' | sed 's/@.*//g' | awk '{print $1}'
 ###
 
-# TAP anything involving 10 (e.g., 1001 - 1099 range).
+# TAP any VM/LXC with an ID containing a specified value (e.g., 1001 - 1099 range).
 ID_RANGE=10
+
 # Virtual SPAN port to mirror traffic to.
 SPAN_PORT='vmbr100'
 
-# Add TAPs for above ID range to a 'network_taps' file.
+# Add TAP(s) for above ID range to a 'network_taps' file.
 ip --brief a | grep "tap\|veth" | sed 's/@.*//g' | grep $ID_RANGE | awk '{print $1}' > network_taps
 
-# OR you can manually include specific ID's.
-#echo tap<id_1>i0 > network_taps
-#echo tap<id_2>i0 >> network_taps
+###
+#    *OR* you can manually include specific ID's.
+#
+#    echo tap<id_1>i0 > network_taps
+#    echo veth<id_2>i0 >> network_taps
+###
 
-echo "Enabling TC Network TAPs:"
+echo "Enabling TC Network TAP(s):"
 
-# Read every TAP interface in 'network_taps' and forward traffic to a designated SPAN port
+# Read every TAP interface in 'network_taps' and forward traffic to the designated SPAN port
 while read i;
 do
     echo "- $i"
+
+    # Capture Ingress Traffic
     tc qdisc add dev $i ingress
     tc filter add dev $i parent ffff: protocol all u32 match u8 0 0 action mirred egress mirror dev $SPAN_PORT
+
+    # Capture Egress Traffic (thx @nukingdragons)
     tc qdisc add dev $i handle 1: root prio
     tc filter add dev $i parent 1: protocol all u32 match u8 0 0 action mirred egress mirror dev $SPAN_PORT
 done < network_taps
